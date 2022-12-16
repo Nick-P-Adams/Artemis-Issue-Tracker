1e190dc05015089bf62bca44aaaf1df5e6873c22
Thedon989 <nick.patrick.adams@gmail.com>
2022-12-16 11:48:10 -0600

    Initial Commit

---
+++ Artemis-Issue-Tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Artemis_Issue_Tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
