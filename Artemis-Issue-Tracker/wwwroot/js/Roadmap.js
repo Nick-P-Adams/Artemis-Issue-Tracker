8f7a3a991961aead06a6959a5c00bf75b8ac5741
Thedon989 <nick.patrick.adams@gmail.com>
2023-03-19 10:44:25 -0500

    Added click to drag functionality to the roadmap. Next I need to clean up the code and seperate into it's own JS file

--- Artemis-Issue-Tracker/wwwroot/js/Roadmap.js
+++ Artemis-Issue-Tracker/wwwroot/js/Roadmap.js
@@ -1,93 +1,158 @@
-﻿const dateContainer = document.getElementById("date-container"),
-      monthNames = ["January", "February", "March", "April", "May", "June",
+﻿const roadmapContainer = document.getElementById("roadmap-container"),
+      dateContainer = document.getElementById("date-container"),
+      months = ["January", "February", "March", "April", "May", "June",
                     "July", "August", "September", "October", "November", "December"],
       weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-let date = new Date(),
-    currentYear = date.getFullYear(),
-    currentMonth = date.getMonth(),
-    lastDateofMonth,
-    lastDateofLastMonth,
-    firstDayofMonth,
-    lastDayofMonth,
-    currentWeek,
-    currentDay,
-    currentDayName;
+let date = new Date(), // todays date
+    year = date.getFullYear(), // getting year based on date
+    month = date.getMonth(), // getting the month based on date
+    lastDateofMonth, // last day number of the month
+    lastDateofLastMonth, // last day number of previous month 
+    firstDayofMonth, // index 0-6 of where the first day of the month occurs in the week
+    lastDayofMonth, // index 0-6 of where the last day of the month occurs in the week
+    currentDate, // index of the date-item that contains the current date
+    week, // week of the month
+    day, // day of month
+    dayNameIndex;
 
 function renderRoadmap() { 
     for (i = 0; i < 12; i++) {
-        if (currentDay == lastDateofMonth - lastDayofMonth) {
-            currentMonth += 1;
+        if (day == lastDateofMonth - lastDayofMonth) {
+            month += 1;
             dateUpdate();
         }
 
-        let dateItem = `<div class="date-item">
-                            <div class="month-container">
-                                <p class="current-month">${monthNames[currentMonth]}</p>
-                            </div>
+        // if needed this can be rearragned to have unique ids for date-item instead of the unordered lists in week-container
+        let dateItemHTML = `<div class="date-item">
+                                <div class="month-container">
+                                     <p class="month-name">${months[month]}</p>
+                                </div>
 
-                            <div class="week-container">
-                                <ul class="day-name-list" id="day-name-list${i}"></ul>
-                                <ul class="day-list" id="day-list${i}"></ul>
-                            </div>
-                        </div>`, // date-item HTML format
-            liDayName = "", x// day-name-list items
+                                <div class="week-container">
+                                    <ul class="day-name-list" id="day-name-list${i}"></ul>
+                                    <ul class="day-list" id="day-list${i}"></ul>
+                                </div>
+                            </div>`, // date-item HTML format
+            liDayName = "", // day-name-list items
             liDay = ""; // day-list items
 
-        dateContainer.innerHTML += dateItem; // adding the date-item to the date-container
+        dateContainer.innerHTML += dateItemHTML; // adding the date-item to the date-container
 
         const dayNameList = document.getElementById(`day-name-list${i}`),
               dayList = document.getElementById(`day-list${i}`); // fetching the current day-name-list and day-list associated with current date-item
-                                                               // there is probably a better way to do this maybe assign the i value to the date-item
-        console.log(currentWeek);
-        if (currentWeek == 0) { // for first week only
-            console.log("here");
+
+        if (week == 0) { // for first week only
             for (let i = firstDayofMonth; i > 0; i--) {
-                liDayName += `<li>${weekDays[currentDayName]}</li>`;
+                liDayName += `<li>${weekDays[dayNameIndex]}</li>`;
                 liDay += `<li>${(lastDateofLastMonth - i) + 1}</li>`; // getting the previous months days that occur in this months first week
-                currentDayName += 1;
+                dayNameIndex += 1;
             }
             
             for (let i = firstDayofMonth; i < 7; i++) {
-                liDayName += `<li>${weekDays[currentDayName]}</li>`;
-                liDay += `<li>${currentDay}</li>`;
-                currentDayName += 1;
-                currentDay += 1;
+                liDayName += `<li>${weekDays[dayNameIndex]}</li>`;
+                liDay += `<li>${day}</li>`;
+                dayNameIndex += 1;
+                day += 1;
             }
         }
         else { // for all the middle weeks 
             for (let i = 0; i < 7; i++) {
                 liDayName += `<li>${weekDays[i]}</li>`;
-                liDay += `<li>${currentDay}</li>`;
-                currentDay += 1;
+                liDay += `<li>${day}</li>`;
+                day += 1;
             }
         }
 
-        currentWeek += 1; // updating the week number for current month
-        currentDayName = 0; // setting index back to 0 for the start of a new week
+        week += 1; // updating the week number for current month
+        dayNameIndex = 0; // setting index back to 0 for the start of a new week
 
         dayNameList.innerHTML = liDayName;
         dayList.innerHTML = liDay;
+
+        if (date.getDate() < day && date.getDate() >= (day - 7) && month === new Date().getMonth()
+            && year === new Date().getFullYear())
+        {
+            currentDate = i+1;
+        }
     }
 }
 
 function dateUpdate() {
-    if (currentMonth < 0 || currentMonth > 11) { // conditions checks to see if we have moved into previous year or next year
+    if (month < 0 || month > 11) { // conditions checks to see if we have moved into previous year or next year
         // creating a new Date object of current year and month then passing it to our date variable
-        date = new Date(currentYear, currentMonth);
-        currentYear = date.getFullYear(); // updating current year with new date year
-        currentMonth = date.getMonth(); // updating current month with new date month
+        date = new Date(year, month);
+        year = date.getFullYear(); // updating current year with new date year
+        month = date.getMonth(); // updating current month with new date month
     }
 
-    currentWeek = 0;
-    currentDay = 1; // gets set back to the first of the month for each new month
-    currentDayName = 0; // the week always starts on sunday // could be a user option later to let the week start on mondays
+    week = 0; // indicating the first week of the month
+    day = 1; // gets set back to the first of the month for each new month
+    dayNameIndex = 0; // the week always starts on sunday // could be a user option later to let the week start on mondays
 
-    lastDateofMonth = new Date(currentYear, currentMonth + 1, 0).getDate(); // fetching last date of month
-    lastDateofLastMonth = new Date(currentYear, currentMonth, 0).getDate(); // fetching last date of previous month
-    firstDayofMonth = new Date(currentYear, currentMonth, 1).getDay(); // fetching index of first day of month
-    lastDayofMonth = new Date(currentYear, currentMonth, lastDateofMonth).getDay(); // fetching index of last day of the month
+    lastDateofMonth = new Date(year, month + 1, 0).getDate(); // last date of month
+    lastDateofLastMonth = new Date(year, month, 0).getDate(); // last date of previous month
+    firstDayofMonth = new Date(year, month, 1).getDay(); // index of first day of month (0-6)
+    lastDayofMonth = new Date(year, month, lastDateofMonth).getDay(); // index of last day of the month (0-6)
 }
 
-dateUpdate();
+dateUpdate(); // initial setup
 renderRoadmap();
+
+// Below is drag to scroll functionality for roadmap-container
+ // Would prefer this to have it's own .js file however I am not sure how to pass values between scripts yet.
+ // In the future I will need to seperate this and pass the position value for current month and date.
+
+let dateItems = document.getElementsByClassName("date-item"),
+    dateItemWidth = $(dateItems[0]).width();
+
+let currentDateItemPosition = ((dateItemWidth * currentDate) - (dateItemWidth / 2)) - ($(roadmapContainer).width() / 2),
+    currentPosition = { left: 0, x: 0 };
+
+const mouseDownHandler = function (e) {
+    pos = {
+        // The current scroll
+        left: roadmapContainer.scrollLeft,
+        // Get the current mouse position
+        x: e.clientX,
+    };
+
+    // Change the cursor and prevent user from selecting the text
+    roadmapContainer.style.cursor = 'grabbing';
+    roadmapContainer.style.userSelect = 'none';
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+};
+
+const mouseMoveHandler = function (e) {
+    // How far the mouse has been moved
+    const dx = e.clientX - pos.x;
+    // Scroll the element
+    roadmapContainer.scrollLeft = pos.left - dx;
+};
+
+const mouseUpHandler = function () {
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+
+    roadmapContainer.style.cursor = 'grab';
+    roadmapContainer.style.removeProperty('user-select');
+};
+
+function scrollToPosition(leftPosition) {
+    roadmapContainer.scrollTo({
+        top: 0,
+        left: leftPosition,
+    });
+}
+function smoothScrollToPosition(leftPosition) {
+    roadmapContainer.scrollTo({
+        top: 0,
+        left: leftPosition,
+        behavior: "smooth",
+    });
+}
+
+scrollToPosition(currentDateItemPosition);
+roadmapContainer.addEventListener('mousedown', mouseDownHandler);
